e3636788bd9e34f5e79e958396f83d8ca5f1fb28
SoniArpit <arpitsoni362@gmail.com>
2020-03-31 21:09:36 +0530

    Using Material Components, adding menu, search, floating button and color change of menu, search icon with white color

--- lib/main.dart
+++ lib/main.dart
@@ -3,58 +3,42 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     title: "My Flutter App",
-    home: MyScaffold(),
+    home: TutorialHome(),
   ));
 }
 
-class MyScaffold extends StatelessWidget {
+class TutorialHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Material(
-      child: Column(
-        children: <Widget>[
-          MyAppBar(
-            title: Text(
-              "Example Text",
-              style: Theme.of(context).primaryTextTheme.title,
-              textAlign: TextAlign.center,
-            ),
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(
+            Icons.menu,
+            color: Colors.white,
           ),
-          Expanded(
-            child: Center(child: Text("Hello World")),
+          tooltip: 'nav menu',
+          onPressed: null,
+        ),
+        title: Text("Example App"),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(
+              Icons.search,
+              color: Colors.white,
+            ),
+            tooltip: 'Search button',
+            onPressed: null,
           )
         ],
       ),
-    );
-  }
-}
-
-class MyAppBar extends StatelessWidget {
-  MyAppBar({this.title});
-  final Widget title;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 80.0,
-      padding: const EdgeInsets.symmetric(horizontal: 8.0),
-      decoration: BoxDecoration(color: Colors.blue[500]),
-      child: Row(
-        children: <Widget>[
-          IconButton(
-            icon: Icon(Icons.menu),
-            tooltip: 'Navigation menu',
-            onPressed: null,
-          ),
-          Expanded(
-            child: title,
-          ),
-          IconButton(
-            icon: Icon(Icons.search),
-            tooltip: 'search',
-            onPressed: null,
-          )
-        ],
+      body: Center(
+        child: Text("Hello Arpit"),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: null,
+        tooltip: 'Add',
+        child: Icon(Icons.add),
       ),
     );
   }
